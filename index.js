4b20698b7df230dd469ed1b70ef12ec6ab5ae0e9
Nicholas Gordon <ngordoncode@gmail.com>
2018-11-06 01:03:06 -0500

    Reworked main page

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'))
-app.listen(3000, () => console.log('Example app listening on port 3000'));
+// At the top of your server.js
+process.env.PWD = process.cwd()
+
+// Then
+app.use(express.static(process.env.PWD + '/public'));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// Routes
+app.get('/', function(req, res) {
+    res.render('index');
+});
 
 app.get('/blog', function(req, res) {
     res.render('blog');
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000')
 });
